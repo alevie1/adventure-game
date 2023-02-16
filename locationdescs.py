70dfb367becff4c6f5c00ff9c849b47636440d70
alevie1 <24alevie@go.dsdmail.net>
2023-02-16 12:25:28 -0700

    location descs finished

--- locationdescs.py
+++ locationdescs.py
@@ -9,7 +9,7 @@ To the west, you can vaguely make out a haze of light."""
 #room2/escape
 	#if all conditions reached
 """You pick your way over the field, your way lit by the lamp 
-and your magical core guarded by the mithral"""
+and your magical core guarded by the adamantine armor."""
 	#if you haven't found the armor
 """Approaching the field of torn metal, you can feel the energy
 of the strange metal as if it is ripping you apart. You back up. Best not to go there yet."""
@@ -30,10 +30,10 @@ a bag filled with strange coins, and an ornate table."""
 		It doesn't feel... right. You weren't always a robot, were you? Come to think of it,
 		you don't really know anything about yourself at all. Strange."""
 	#table
-		"""Beautifully carved from cherry wood. Probably worth a lot before it was tossed here.
+		"""Beautifully carved from a wood-like substance. Probably worth a lot before it was tossed here.
 		Now it's filled with termites."""
 	#coins
-		"""Electrum pieces. You can barely make out the inscription on the face. 'inscription lol' """
+		"""Electrum pieces. You can't make out the inscription on the face."""
 	#box
 		"""You open the box and pull out a shimmering silk cloak, miraculously intact.
 		As you hold it, you notice your hands seem distorted behind it- hard to focus on somehow,
@@ -42,42 +42,72 @@ a bag filled with strange coins, and an ornate table."""
 
 #room5/townsquare
 """A huge square opening near the entrance to an abandoned city. Gorgeous but decrepit buildings line the paths.
-An eerie silence fills the whole place. You can't shake the feeling that you shouldn't be here. 
-The streets sprawl out in front of you."""
+Strange magical phenomena are visible all around- a pillar of purple flame, a group of skittering rats with
+gossamer wings, a strange shadowy figure which you think it best not to approach.
+An eerie silence fills the whole place. The streets sprawl out in front of you."""
 
 #room6/towers
-"""This road is lined with huge spires. They may have once been beautiful.
+"""This road is lined with huge spire-like structures. They may have once been beautiful.
 One of them seems intact enough to enter."""
 
 #room7/labfloor1
-"""A laboratory building, or perhaps a mechanic's shop? Tools line the walls and are scattered across the floor.
-There's a flight of stairs at the back."""
+"""A laboratory building, or perhaps a mechanic's shop? Tools line the 
+walls and are scattered across the floor. There's a flight of stairs at the back."""
 
 #room8/labfloor2
-"""Papers litter the floor. There's a table in the middle which holds dismembered parts, presumably of another warforged.
-A notebook sits next to the scattered screws and plates."""
+"""Papers litter the floor. There's a table in the middle which holds dismembered 
+parts, presumably of another warforged. A notebook sits next to the scattered screws and plates."""
 	#notebook
-	"""Scattered notes, entries from random dates. Read them?"""
+	"""Notes and lab entries from random dates. Read them?"""
 	#yes
-		"""Entry no. 1: It's hard work here. Sometimes I wonder if all this 'progress' is worth it.
+		"""Entry no. 20: It's hard work here. Sometimes I wonder if all this 'progress' is worth it.
 		Is the name of science really worth the things we've done? Another soul lost today, and no closer to perfecting the process."""
 			#keep reading?
 
-		"""Entry no. 450: We're making progress, at last. Today, we managed to transfer a single soul, subject 47, into a crystal,
-		using the precise rituals we've devised. We believe this to be a breakthrough. """
+		"""Entry no. 139: The secret, it seems, lies in the crystals from the dead god's body. If the soul magic can be properly
+		channelled, we could make something of it."""
+			#keep reading?
+
+		"""Entry no. 450: We're making progress, at last. Today, we managed to transfer a single soul, Subject 47, into a crystal,
+		using the precise rituals we've devised. We believe this to be a breakthrough."""
+			#keep reading?
 
 		"""Entry no. 451: Subject 47 became dangerous and was terminated immediately, as per standard protocol. The ritual must be altered."""
 			#keep reading?
 
-		"""Entry no. 1096: Today, the first of the crystal-beings awakened at last. We've been calling them 'warforged' although
-		their technical name is ''. The crystal seems to allow the soul inside full control over the mechanical body.
+		"""Entry no. 1096: Today, the first of the crystal-beings awakened at last. We've begun to call them 'warforged'. A bloody name,
+		but I fear it will prove all too accurate. The crystal seems to allow the soul inside full control over the mechanical body.
 		No adverse effects so far. Could this be the secret to immortality?"""
 			#keep reading?
 
-		"""Entry no. 1549: Should we be allowed immortality? The impact of all the things I've done weighs on me more each day.
+		"""Entry no. 1549: Should we even be allowed immortality? The impact of all the things I've done weighs on me more each day.
 		The warforged grow ever more advanced, our successes ever more marvelous. The failures, though, grow ever more devastating.
 		All these experiments in the name of progress... I fear that this will be our downfall."""
 			#keep reading?
 
-		"""Entry no. 1550: This will be the last entry I write, I fear. The council suspects a revolt, and if they find my involvement
-		with the resistance efforts here, 
+		"""Entry no. 1550: This will be the last entry I write, I fear. The council suspects a revolt, and if they discover my recent
+		involvement with the resistance efforts here, my work could be in danger. There's a drow city a couple days to the north.
+		Perhaps I can find refuge there."""
+			#keep reading?
+		"""The rest of the book is blank."""
+		"""Under the notebook is a scrap of paper with a code written on it: [code lol]."""
+
+#room9/mindflayers
+"""It seems a group of illithids have set up a base in this part of town. You try to get past, 
+but they sense your consciousness and drive you away."""
+
+#room10/armory
+"""An armory, filled with weapons and armor. Most of it is rusted beyond repair, but you catch a gleam
+of metal near the back."""
+	#armor
+	"""It's a suit of mithral armor. You think it might be able to protect your magical essence."""
+
+#room11/crater
+"""An enormous crater."""
+
+#room12
+"""There's a metal door built into the ground. Looks like you need a password to enter."""
+
+#room13
+"""Looks like the interior of a fallout bunker. Evidently it didn't protect its inhabitants well enough-
+a skeleton lies in the corner. Shelves line the back wall- historical records, it appears."""
