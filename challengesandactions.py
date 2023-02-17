4e1bc269ec64cf64db9db11ec3ac4269097fdbc8
alevie1 <24alevie@go.dsdmail.net>
2023-02-17 11:13:32 -0700

    e

---
+++ challengesandactions.py
@@ -0,0 +1,14 @@
+#challenges and actions for game
+#main goal
+	#collect all memories in order to complete your crystal and put the city's inhabitants to rest
+#challenges
+	#exploration and discovery of locations and crystal fragments
+#probably only need 3 to escape, but if you collect all 5 that are hidden, you can unlock a 'secret ending'?
+#actions
+	#move cardinal directions
+	#use items
+	#take items
+	#look at/read things
+	#talk with npcs? let you type something and if you use their 'favorite' or 'least favorite' words it'll affect interactions?
+	#equip items???
+#items
--- locationdescs.py
+++ locationdescs.py
@@ -12,8 +12,8 @@ To the west, you can vaguely make out a haze of light."""
 and your magical core guarded by the adamantine armor."""
 	#if you haven't found the armor
 """Approaching the field of torn metal, you can feel the energy
-of the strange metal as if it is ripping you apart. You back up. Best not to go there yet."""
-	#if you haven't found the light
+of the strange metal as if it is ripping your very soul apart. You back up. Best not to go there yet."""
+	#if you haven't found the (light maybe?? unsure. other condition)
 """It's too dark to see anything."""
 
 #room3/travel
@@ -21,7 +21,7 @@ of the strange metal as if it is ripping you apart. You back up. Best not to go 
 Absently, you wonder what could have damaged it so badly."""
 
 #room4/scraps
-"""A field of random junk. The items that most attract your interest are an old box, a silver mirror,
+"""A field of random junk. The items that most interest you are an old box, a silver mirror,
 a bag filled with strange coins, and an ornate table."""
 	#items - flavor text
 	#mirror
@@ -92,6 +92,18 @@ parts, presumably of another warforged. A notebook sits next to the scattered sc
 		"""The rest of the book is blank."""
 		"""Under the notebook is a scrap of paper with a code written on it: [code lol]."""
 
+#labbasement
+"""A robot like you sits in the corner, mostly dismantled. Its eyes glow as it wakes up. You hear a voice in your head."""
+	#robot lines
+		"""Hello. It's been awhile since - - -an --yone came down here. I am - --- E -- Es Ther - -- """
+		"""I h-- --- -- hav --e something yo- u are lll---l-looking for"""
+		"""If youc can hel-p me f--f-fix my body. W-w-w-What do you say? Is it a- --D-eAl?"""
+	#if they talk to it, it offers them a crystal in exchange for helping to rebuild its body
+	#if they agree - crystal1
+	#Esther can later be found standing in the second floor, busily working on something. She'll talk to you but not much.
+		"""You helped Esther to put her body back together. She leaves, setting something on the floor."""
+		"""A crystal. It glows blue. Holding it, you remember something..."""
+
 #room9/mindflayers
 """It seems a group of illithids have set up a base in this part of town. You try to get past, 
 but they sense your consciousness and drive you away."""
@@ -101,6 +113,11 @@ but they sense your consciousness and drive you away."""
 of metal near the back."""
 	#armor
 	"""It's a suit of mithral armor. You think it might be able to protect your magical essence."""
+	#if they try to take more stuff - crystal5
+	"""You notice a small door behind the racks. You open it. Inside is a blue crystal."""
+	#if they take it
+	"""A crystal. It glows blue. Holding it, you remember something..."""
+
 
 #room11/crater
 """An enormous crater."""
@@ -111,3 +128,45 @@ of metal near the back."""
 #room13
 """Looks like the interior of a fallout bunker. Evidently it didn't protect its inhabitants well enough-
 a skeleton lies in the corner. Shelves line the back wall- historical records, it appears."""
+	#The records have things in them
+	#If you try to take something
+	"""Something glints blue from the skeleton's hand."""
+
+#catacombs under lab
+	"""As you descend deeper into the basement, into the very bowels of the city, you can feel a presence.
+	Something is wrong here. At last, you reach flat ground in a tunnel. You catch a glimpse of something
+	glowing ahead."""
+
+#tomb
+	"""A huge cavern. In the center lies a massive body, made of a stone-like substance. Dried oil-like
+	blood cakes the ground beneath it, and huge sections are missing or collapsed. The inside is filled
+	with blue crystals, which it appears were being mined from the body. You can feel one calling to you."""
+
+
+#memories
+#1
+	"""You remember your name. Or at least, you think it was yours. Rowena. You can hear the deep voice
+	saying it, and know it was someone important. Someone you loved."""
+#this one is only for the tomb
+	"""You remember this place. This... figure. It was once a god. A keeper of justice. You remember the
+	day you first entered one of its temples, and you remember the day that your people clawed their way
+	to its home plane. You remember the feeling as ichor spilled onto the ground. The feeling that nothing
+	would ever be right again."""
+#2
+	"""You remember becoming a warforged. The soul magic was painful, but you knew you could now live forever.
+	You were grateful for the immortality at the time- who knew what awaited in the afterlife after the god's death?"""
+#3
+	"""You remember a dwarven man, with a deep voice, whom you loved more than anything. His name was Hector. You remember
+	watching him grow old, seeing his eyes as the life faded from them, because even the hundreds of years dwarves
+	live are nothing next to the solitary, eternal existence of a warforged."""
+#4
+	"""You remember entering stasis after a catastrophic event, after your city had collapsed on itself.
+	You remember intending to awaken only when you were needed. Clearly, that time is now."""
+#this one activates after all 5 crystals have been found
+	"""Finally, you remember everything. Your life, the day the revolts happened- the people, tired of
+	living under the oppressive government built on the corpse of justice, the oligarchy, ready to
+	end it all if they couldn't have all the power for themselves, and you remember what you need to do.
+	You remember the runes that will allow the god's restless, trapped spirit to be laid to rest at last.
+	The runes that will restore balance and let all the other souls trapped within this forgotten city
+	to be free. The runes inscribed on your own metal body. You know, beyond any doubt, that this is what
+	you designed yourself for. To put right what was wrong at last."""
